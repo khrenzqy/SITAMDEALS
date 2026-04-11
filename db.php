38f7ea2dfc0dd4635ca0239cc32055214e6954aa
nayla <nayla.arviandini@gmail.com>
2026-04-11 20:21:39 +0700

    tambahin db

--- db.php
+++ db.php
@@ -31,4 +31,7 @@ $koneksi = mysqli_connect($host, $user, $password, $database);
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
+// Optional: set charset biar aman
+mysqli_set_charset($koneksi, "utf8");
 ?>
